d0921c1233aceaf87c1972366fc69ef000824c62
cssopy <83584721+cssopy@users.noreply.github.com>
2022-08-01 03:54:57 +0900

    [BE] 생성 : 댓글 생성 및 조회
    댓글 생성 및 조회 기능 추가,
    Comment user 외래키 지정으로 수정

--- back-end/five/src/main/java/com/ssafy/five/domain/repository/CmtRepository.java
+++ back-end/five/src/main/java/com/ssafy/five/domain/repository/CmtRepository.java
@@ -1,7 +1,16 @@
 package com.ssafy.five.domain.repository;
 
+import com.ssafy.five.domain.entity.Board;
 import com.ssafy.five.domain.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CmtRepository extends JpaRepository<Comment, Long> {
+
+    @Modifying
+    @Query("SELECT c from Comment c WHERE c.board = :board")
+    List<Comment> findALLByBoardId(Board board);
 }
