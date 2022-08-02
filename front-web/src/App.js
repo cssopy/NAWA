74c71d2358a5a34f52971db6c4e0f13b434e4666
JaeHyuk Han <jhhan2206@naver.com>
2022-08-02 16:46:00 +0900

    새로고침시 스크롤 맨위로 옮기는 작업

--- front-web/src/App.js
+++ front-web/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbartop from './router/Navbar';
 import Home from './router/Home';
@@ -9,25 +9,33 @@ import Sigup from './router/Signup'
 import ScrollToTop from "react-scroll-to-top";
 
 function App() {
-    return (
-        <BrowserRouter>
-            <Navbartop />
-            <Routes>
-                <Route exact path='/' element={<Home />} />
-                <Route exact path='/intro' element={<Intro />} />
-                <Route exact path='/contact' element={<Contact />} />
-                <Route exact path='/login' element={<Login />} />
-                <Route exact path='/signup' element={<Sigup />} />
-            </Routes>
-            <ScrollToTop
-              smooth
-              color="beige"
-              style={{
-                backgroundColor: 'violet',
-              }}
-            />
-        </BrowserRouter>
-    )
+  // 새로고침 시 스크롤을 맨 위로 올리게 하는 effect
+  // 추후 새로고침해도 변경이 일어나지 않게 하려면 여길 없애면 된다
+  useEffect(() => {
+    window.onbeforeunload = function pushRefresh() {
+      window.scrollTo(0, 0);
+    };
+  }, []);
+
+  return (
+    <BrowserRouter>
+      <Navbartop />
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route exact path='/intro' element={<Intro />} />
+        <Route exact path='/contact' element={<Contact />} />
+        <Route exact path='/login' element={<Login />} />
+        <Route exact path='/signup' element={<Sigup />} />
+      </Routes>
+      <ScrollToTop
+        smooth
+        color="beige"
+        style={{
+          backgroundColor: 'violet',
+        }}
+      />
+    </BrowserRouter>
+  )
 }
 
 export default App;
