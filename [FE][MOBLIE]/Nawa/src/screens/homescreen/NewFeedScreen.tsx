0eb33931063db61696b8b404f7fa0a0446edf228
JaeHyuk Han <jhhan2206@naver.com>
2022-08-10 17:03:00 +0900

    newfeed 라이브러리 작동 확인 & formdata에 데이터 병합 성공

--- [FE][MOBLIE]/Nawa/src/screens/homescreen/NewFeedScreen.tsx
+++ [FE][MOBLIE]/Nawa/src/screens/homescreen/NewFeedScreen.tsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
-import { SafeAreaView, ScrollView, Dimensions, StyleSheet, Alert, Platform } from "react-native";
+import { ScrollView, Dimensions, StyleSheet, Alert, Platform, View } from "react-native";
 
 import { Form, FormItem } from 'react-native-form-component';
-import { Button } from "@rneui/base";
-import { launchImageLibrary } from "react-native-image-picker"
+import { Button, ScreenHeight } from "@rneui/base";
+import { launchCamera, launchImageLibrary } from "react-native-image-picker"
 import axios, { AxiosError } from "axios";
 import { useSelector } from "react-redux";
 import { RootState } from "../../store/reducer";
@@ -14,26 +14,26 @@ const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get("window");
 function NewFeedScreen({ navigation }) {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
-  const [img, setImg] = useState([]);
-  const [imgVisible, setImgVisible] = useState(false);
   const [loading, setLoading] = useState(false)
 
-  const whoamI = useSelector((state : RootState) => state.user.userId)
+  // const whoamI = useSelector((state : RootState) => state.user.userId)
+  const whoamI = 'imageupload12'
+
+  let getMedia;
+
+  const formdata = new FormData();
 
   const onSubmit = async () => {
-    // const formdata = new FormData();
+    console.log(getMedia)
     if (loading) return;
     try {
-      const formdata = new FormData();
       formdata.append("key", new Blob([JSON.stringify({
         boardContent: content,
         boardTitle: title,
         userId: whoamI
       })]))
-      // for (let i = 0; i < imgUrl.length; i++){
-      //   formdata.append("uploadfile", imgUrl[i]);
-      // }
       setLoading(true);
+      console.log(formdata)
       await axios.post('http://i7d205.p.ssafy.io:8080/board',{
         formdata,
         headers: {
@@ -51,63 +51,140 @@ function NewFeedScreen({ navigation }) {
     navigation.navigate('Main')
   }
 
-  const pickLibraryImage = () => {
-    launchImageLibrary({
-      mediaType: "mixed",
-      includeBase64: Platform.OS === 'android',
-    }, (res) => {
-      console.log(res.assets);
-      if (res.didCancel) return;
-      setImgVisible(true)
+  const openCamera = () => {
+    const option = {
+      mediaType: 'photo',
+      includeBase64: true,
+      quality: 1
+    }
+    
+    launchCamera(option, (res) => {
+      if(res.didCancel) {
+        console.log('User Cancelled image picker')
+      } else if(res.errorCode) {
+        console.log(res.errorMessage)
+      } else {
+        const data = res.assets
+        const media = {
+          uri: data[0].uri,
+          type: data[0].type,
+          name: data[0].fileName,
+        }
+        getMedia = media
+      }
+    })
+  }
+
+  const openVideo = () => {
+    const option = {
+      mediaType: 'video',
+      quality: 1
+    }
+    
+    launchCamera(option, (res) => {
+      if(res.didCancel) {
+        console.log('User Cancelled image picker')
+      } else if(res.errorCode) {
+        console.log(res.errorMessage)
+      } else {
+        const data = res.assets
+        const media = {
+          uri: data[0].uri,
+          type: data[0].type,
+          name: data[0].fileName,
+        }
+        getMedia = media
+      }
+    })
+  }
+
+  const openStorage = () => {
+    const option = {
+      mediaType: 'mixed',
+      includeBase64: true,
+    }
+
+    launchImageLibrary(option, (res) => {
+      if(res.didCancel) {
+        console.log('User Cancelled image picker')
+      } else if(res.errorCode) {
+        console.log(res.errorMessage)
+      } else {
+        const data = res.assets
+        const media = {
+          uri: data[0].base64,
+          type: data[0].type,
+          name: data[0].fileName,
+        }
+        console.log(media)
+        getMedia = media
+        formdata.append('uploadfile', media)
+      }
     })
   }
 
   return (
-    <SafeAreaView
+    <ScrollView
       style={styles.safe}
     >
-      {/* {true &&
-        <Image source={{uri: }}/>
-      } */}
-      <ScrollView>
-        <Form
-          onButtonPress={onSubmit}
-          buttonText = "보내기"
-          >
-          <Button
-            title="갤러리"
-            onPress={() => pickLibraryImage}
-            containerStyle={{
-              width: SCREEN_WIDTH * 0.8,
-              marginBottom: SCREEN_HEIGHT * 0.05,
-            }}
-          />
-          <FormItem
-            textArea
-            label="제목"
-            value={title}
-            returnKeyType="next"
-            placeholder="제목을 입력해주세요"
-            onChangeText={setTitle}
-          />
-          <FormItem
-            textArea
-            label="내용"
-            value={title}
-            returnKeyType="send"
-            placeholder="내용을 입력해주세요"
-            onChangeText={setContent}
-          />
-        </Form>
-      </ScrollView>
-    </SafeAreaView>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: 'space-evenly',
+          marginBottom: SCREEN_HEIGHT * 0.05
+        }}
+      >
+      
+      <Button
+        title="사진"
+        onPressIn={openCamera}
+        containerStyle={styles.button}
+      />
+
+      <Button
+        title="동영상"
+        onPressIn ={openVideo}
+        containerStyle={styles.button}
+      />
+
+      <Button
+        title="갤러리"
+        onPressIn={openStorage}
+        containerStyle={styles.button}
+      />
+      </View>
+      <Form
+        onButtonPress={onSubmit}
+        buttonText = "보내기"
+        >
+        <FormItem
+          textArea
+          label="제목"
+          value={title}
+          returnKeyType="next"
+          placeholder="제목을 입력해주세요"
+          onChangeText={setTitle}
+        />
+        <FormItem
+          textArea
+          label="내용"
+          value={content}
+          returnKeyType="send"
+          placeholder="내용을 입력해주세요"
+          onChangeText={setContent}
+        />
+      </Form>
+    </ScrollView>
   )
 }
 
 const styles = StyleSheet.create({
   safe: {
-    marginTop: SCREEN_HEIGHT * 0.1,
-    marginHorizontal: SCREEN_WIDTH * 0.1,
+    paddingVertical: ScreenHeight * 0.05,
+    paddingHorizontal: SCREEN_WIDTH * 0.1,
+  },
+  button: {
+    width: SCREEN_WIDTH * 0.2,
   }
 });
 export default NewFeedScreen
