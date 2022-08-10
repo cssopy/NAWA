56f37ce9fa6e09f0cf9bbdceadb86d6f54d3c6ad
JaeHyuk Han <jhhan2206@naver.com>
2022-08-10 15:06:34 +0900

    formdata make

--- [FE][MOBLIE]/Nawa/src/screens/homescreen/NewFeedScreen.tsx
+++ [FE][MOBLIE]/Nawa/src/screens/homescreen/NewFeedScreen.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
-import { SafeAreaView, ScrollView, Dimensions, StyleSheet, Image, Platform, Alert } from "react-native";
+import { SafeAreaView, ScrollView, Dimensions, StyleSheet, Alert, Platform } from "react-native";
 
 import { Form, FormItem } from 'react-native-form-component';
-import { launchCamera, launchImageLibrary } from 'react-native-image-picker';
 import { Button } from "@rneui/base";
+import { launchImageLibrary } from "react-native-image-picker"
 import axios, { AxiosError } from "axios";
+import { useSelector } from "react-redux";
+import { RootState } from "../../store/reducer";
 
 const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get("window");
 
@@ -12,30 +14,36 @@ const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get("window");
 function NewFeedScreen({ navigation }) {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
-  const [imgUrl, setImgUrl] = useState([]);
+  const [img, setImg] = useState([]);
   const [imgVisible, setImgVisible] = useState(false);
   const [loading, setLoading] = useState(false)
 
+  const whoamI = useSelector((state : RootState) => state.user.userId)
 
   const onSubmit = async () => {
     // const formdata = new FormData();
     if (loading) return;
     try {
+      const formdata = new FormData();
+      formdata.append("key", new Blob([JSON.stringify({
+        boardContent: content,
+        boardTitle: title,
+        userId: whoamI
+      })]))
+      // for (let i = 0; i < imgUrl.length; i++){
+      //   formdata.append("uploadfile", imgUrl[i]);
+      // }
       setLoading(true);
-      const response = await axios.post('http://i7d205.p.ssafy.io:8080/board',{
-        key: {
-          boardContent: content,
-          boardTitle: title,
-          // userId: 
-        },
-        uploadfile: {
-
+      await axios.post('http://i7d205.p.ssafy.io:8080/board',{
+        formdata,
+        headers: {
+          'Content-Type': 'multipart/form-data'
         }
       });
     } catch (error) {
       const errorResponse = (error as AxiosError).response;
       if (errorResponse) {
-        Alert.alert('알림', 'errorResponse.data.message')
+        Alert.alert('오류가 발생했습니다');
       }
     } finally {
       setLoading(false)
@@ -76,10 +84,10 @@ function NewFeedScreen({ navigation }) {
           />
           <FormItem
             textArea
-            label="내용"
+            label="제목"
             value={title}
-            returnKeyType="send"
-            placeholder="내용을 입력해주세요"
+            returnKeyType="next"
+            placeholder="제목을 입력해주세요"
             onChangeText={setTitle}
           />
           <FormItem
@@ -88,7 +96,7 @@ function NewFeedScreen({ navigation }) {
             value={title}
             returnKeyType="send"
             placeholder="내용을 입력해주세요"
-            onChangeText={setTitle}
+            onChangeText={setContent}
           />
         </Form>
       </ScrollView>
