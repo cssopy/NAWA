188c168fb25139e70730e1273d395e517b9dd795
Han JaeHyuk <jhhan2206@naver.com>
2022-08-11 10:32:07 +0900

    ready to communicate

--- [FE][MOBLIE]/Nawa/src/screens/homescreen/NewFeedScreen.tsx
+++ [FE][MOBLIE]/Nawa/src/screens/homescreen/NewFeedScreen.tsx
@@ -3,7 +3,7 @@ import { ScrollView, Dimensions, StyleSheet, Alert, Platform, View } from "react
 
 import { Form, FormItem } from 'react-native-form-component';
 import { Button, ScreenHeight } from "@rneui/base";
-import { launchCamera, launchImageLibrary } from "react-native-image-picker"
+import { launchCamera, launchImageLibrary } from "react-native-image-picker";
 import axios, { AxiosError } from "axios";
 import { useSelector } from "react-redux";
 import { RootState } from "../../store/reducer";
@@ -14,42 +14,14 @@ const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get("window");
 function NewFeedScreen({ navigation }) {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
+  const [media, setMedia] = useState(Object);
   const [loading, setLoading] = useState(false)
 
   // const whoamI = useSelector((state : RootState) => state.user.userId)
+  const myId = useSelector((state: RootState) => state.user.accessToken)
   const whoamI = 'imageupload12'
 
-  let getMedia;
-
   const formdata = new FormData();
-
-  const onSubmit = async () => {
-    console.log(getMedia)
-    if (loading) return;
-    try {
-      formdata.append("key", new Blob([JSON.stringify({
-        boardContent: content,
-        boardTitle: title,
-        userId: whoamI
-      })]))
-      setLoading(true);
-      console.log(formdata)
-      await axios.post('http://i7d205.p.ssafy.io:8080/board',{
-        formdata,
-        headers: {
-          'Content-Type': 'multipart/form-data'
-        }
-      });
-    } catch (error) {
-      const errorResponse = (error as AxiosError).response;
-      if (errorResponse) {
-        Alert.alert('오류가 발생했습니다');
-      }
-    } finally {
-      setLoading(false)
-    }
-    navigation.navigate('Main')
-  }
 
   const openCamera = () => {
     const option = {
@@ -65,20 +37,21 @@ function NewFeedScreen({ navigation }) {
         console.log(res.errorMessage)
       } else {
         const data = res.assets
-        const media = {
+        const callMedia = {
           uri: data[0].uri,
           type: data[0].type,
           name: data[0].fileName,
         }
-        getMedia = media
+        setMedia(callMedia)
       }
     })
   }
 
   const openVideo = () => {
     const option = {
+      includeBase64: true,
       mediaType: 'video',
-      quality: 1
+      quality: 1,
     }
     
     launchCamera(option, (res) => {
@@ -88,12 +61,12 @@ function NewFeedScreen({ navigation }) {
         console.log(res.errorMessage)
       } else {
         const data = res.assets
-        const media = {
+        const callMedia = {
           uri: data[0].uri,
           type: data[0].type,
           name: data[0].fileName,
         }
-        getMedia = media
+        setMedia(callMedia)
       }
     })
   }
@@ -101,7 +74,6 @@ function NewFeedScreen({ navigation }) {
   const openStorage = () => {
     const option = {
       mediaType: 'mixed',
-      includeBase64: true,
     }
 
     launchImageLibrary(option, (res) => {
@@ -110,17 +82,48 @@ function NewFeedScreen({ navigation }) {
       } else if(res.errorCode) {
         console.log(res.errorMessage)
       } else {
-        const data = res.assets
-        const media = {
-          uri: data[0].base64,
-          type: data[0].type,
-          name: data[0].fileName,
+        console.log(typeof(res.assets[0]))
+        const data = res.assets[0]
+        // console.log(data)
+        const callMedia = {
+          uri: Platform.OS === 'android' ? data.uri?.replace('file://', '') : data.uri,
+          type: data.type,
+          name: data.fileName,
         }
-        console.log(media)
-        getMedia = media
-        formdata.append('uploadfile', media)
+        setMedia(callMedia)
+        console.log(callMedia)
       }
     })
+  }
+  
+  const onSubmit = async () => {
+    if (loading) return;
+    try {
+      formdata.append("key", new Blob([JSON.stringify({
+        boardContent: content,
+        boardTitle: title,
+        userId: whoamI
+      })]))
+      setLoading(true);
+      console.log(formdata)
+      await axios.post('http://i7d205.p.ssafy.io:8080/board',{
+        formdata,
+        headers: {
+          'Content-Type': 'multipart/form-data',
+          'Authorization': myId,
+        }
+      }).then((res) => {
+        console.log(res)
+      });
+    } catch (error) {
+      const errorResponse = (error as AxiosError).response;
+      if (errorResponse) {
+        Alert.alert('오류가 발생했습니다');
+      }
+    } finally {
+      setLoading(false)
+    }
+    navigation.navigate('Main')
   }
 
   return (
