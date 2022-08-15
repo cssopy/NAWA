e89d3008fb9e34bddd2e4dc713286b501c54dcf4
mosquito0076 <hsmk0076@naver.com>
2022-08-15 20:39:17 +0900

    =[BE] 수정 : 채팅 수정

--- back-end/five/src/main/java/com/ssafy/five/config/stomp/WebConfig.java
+++ back-end/five/src/main/java/com/ssafy/five/config/stomp/WebConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedHeaders("*")
+        registry.addMapping("/**").allowedHeaders("*").allowCredentials(true)
                 .allowedOrigins("*");
     }
 }
