1b394fe96c03db529de7e8c86111fb0eee2b0281
Juwon-Kiim <sa6812000@naver.com>
2022-08-16 02:22:15 +0900

    [BE] 수정 : FileDto 수정

--- back-end/five/src/main/java/com/ssafy/five/controller/dto/FileDto.java
+++ back-end/five/src/main/java/com/ssafy/five/controller/dto/FileDto.java
@@ -2,13 +2,13 @@ package com.ssafy.five.controller.dto;
 
 import com.ssafy.five.domain.entity.EnumType.FileType;
 import com.ssafy.five.domain.entity.Files;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 @Getter
 @Setter
 @ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class FileDto {
 
     private Long boardId;
