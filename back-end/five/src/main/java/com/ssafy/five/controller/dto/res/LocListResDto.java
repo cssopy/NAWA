afeecd614a414b21fd75b76b9a4e0b9ae179cd5d
cssopy <83584721+cssopy@users.noreply.github.com>
2022-08-16 00:03:59 +0900

    [BE] 수정 : locId 반환

--- back-end/five/src/main/java/com/ssafy/five/controller/dto/res/LocListResDto.java
+++ back-end/five/src/main/java/com/ssafy/five/controller/dto/res/LocListResDto.java
@@ -5,16 +5,14 @@ import lombok.Getter;
 
 @Getter
 public class LocListResDto {
-
     private String userId;
-
+    private Long locId;
     private String locAddress;
-
     private double locLat;
-
     private double locLng;
 
     public LocListResDto(LocationList locationListEntity) {
+        this.locId = locationListEntity.getLocId();
         this.userId = locationListEntity.getUser().getUserId();
         this.locAddress = locationListEntity.getLocAddress();
         this.locLat = locationListEntity.getLocLat();
