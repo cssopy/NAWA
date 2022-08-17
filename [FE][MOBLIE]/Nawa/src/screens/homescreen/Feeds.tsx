8de6c14e90a93460a32796a9c6fd10f94ca5f1d8
JaeHyuk Han <jhhan2206@naver.com>
2022-08-17 15:39:30 +0900

    infinite scroll

--- [FE][MOBLIE]/Nawa/src/screens/homescreen/Feeds.tsx
+++ [FE][MOBLIE]/Nawa/src/screens/homescreen/Feeds.tsx
@@ -1,5 +1,5 @@
 import React, { useRef, useEffect, useState, useCallback } from "react";
-import { Animated, Dimensions, StyleSheet, Text, TouchableWithoutFeedback, View, Image, Button, ActivityIndicator, RefreshControl, Alert } from "react-native";
+import { Animated, Dimensions, StyleSheet, Text, TouchableWithoutFeedback, View, Image, Button, ActivityIndicator, RefreshControl, Alert, FlatList } from "react-native";
 
 import axios from 'axios';
 import { FAB } from "@rneui/base";
@@ -18,14 +18,8 @@ const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get("window");
 const HEADER_HEIGHT = SCREEN_HEIGHT * 0.05;
 
 function Feeds ({ navigation }) {
-
-  const [feeds, setFeeds] = useState<object[]>([]);
-  const [page, setPage] = useState<number>(0);
   // const [totalFeeds, setTotalFeeds] = useState<number>(0);
-  const [loading, setLoading] = useState<boolean>(false);
-  const [noMoreFeed, setNoMoreFeed] = useState<boolean>(false);
-  const [ticTok, setTicTok] = useState<boolean>(false);
-  const [refreshing, setReFreshing] = useState<boolean>(false);
+  
 
   const isFocused = useIsFocused()
   
@@ -38,52 +32,6 @@ function Feeds ({ navigation }) {
     inputRange: [0, 1],
     outputRange: [0, -HEADER_HEIGHT],
   });
-
-  const getfeeds = () => {
-    const sendUrl = `${url}board/mainFeed/NEW/${page}`
-    setLoading(true)
-    axios.get(
-      sendUrl,
-      { headers: { Authorization : `Bearer ${myId}` }}
-    ).then(res => {
-      setFeeds([...feeds, ...res.data])
-      setPage(page + res.data.length)
-    }).catch(err => Alert.alert('알림', `오류 발생 ${err}`))
-    setLoading(false)
-  }
-    
-  useEffect(() => {
-    // console.log('access token', myId)
-    // const sendUrl = `${url}board/`
-
-    // 현재 피드의 총 갯수 확인
-    // axios.get(
-    //   'http://i7d205.p.ssafy.io/api/board/count',
-    //   { headers: { Authorization : `Bearer ${myId}` }}
-    // ).then(res=> setTotalFeeds(res.data))
-
-    // refresh 해제
-    setReFreshing(false)
-
-    // 초기 10개의 피드 호출
-    getfeeds();
-
-  },[isFocused, ticTok])
-  
-  const checkfeed = async () => {
-    if (!loading) {
-      if (page >= 10 && !noMoreFeed) {
-        getfeeds();
-      }
-    }
-  }
-
-  const refresh = () => {
-    setReFreshing(true)
-    console.log('refresh')
-    setPage(0)
-    getfeeds()
-  }
 
   const dispatch = useAppDispatch();
   const logOutHAzaJaHuckAAAAAAAAAA = async () => {
@@ -102,8 +50,73 @@ function Feeds ({ navigation }) {
 
 
   const MainFeed = () => {
+    const [feeds, setFeeds] = useState<object[]>([]);
+    const [page, setPage] = useState<number>(0);
+    const [loading, setLoading] = useState<boolean>(false);
+    const [noMoreFeed, setNoMoreFeed] = useState<boolean>(false);
+    const [ticTok, setTicTok] = useState<boolean>(false);
+    const [refreshing, setReFreshing] = useState<boolean>(false);
+
     let fileUrl:string;
     let fileType:string;
+
+    console.log('data count', feeds.length)
+
+    const getfeeds = () => {
+      const sendUrl = `${url}board/mainFeed/NEW/${page}`
+      console.log('sending', sendUrl)
+      axios.get(
+        sendUrl,
+        { headers: { Authorization : `Bearer ${myId}` }}
+      ).then(res => {
+        if (res.data.length < 10 ) {
+          setNoMoreFeed(true)
+        }
+        setFeeds(feeds.concat(res.data))
+        setPage(page + res.data.length)
+      }).catch(err => Alert.alert('알림', `오류 발생 ${err}`))
+    }
+      
+    useEffect(() => {
+      // console.log('access token', myId)
+      // const sendUrl = `${url}board/`
+  
+      // 현재 피드의 총 갯수 확인
+      // axios.get(
+      //   'http://i7d205.p.ssafy.io/api/board/count',
+      //   { headers: { Authorization : `Bearer ${myId}` }}
+      // ).then(res=> setTotalFeeds(res.data))
+  
+      // refresh 해제
+      setLoading(true)
+      
+      // 초기 10개의 피드 호출
+      getfeeds();
+      
+      setReFreshing(false)
+      setNoMoreFeed(false)
+      setLoading(false)
+  
+    },[isFocused, ticTok])
+    
+    const checkfeed = async () => {
+      if (!loading) {
+        console.log('더 없나?', page, noMoreFeed)
+        if (page >= 10 && !noMoreFeed) {
+          setTicTok(!ticTok)
+        } else {
+          console.log('더 없네')
+        }
+      }
+    }
+  
+    const refresh = () => {
+      setReFreshing(true)
+      console.log('refresh')
+      setPage(0)
+      getfeeds()
+      setReFreshing(false)
+    }
 
     const onPressListHandler = (data: object) => {
       navigation.navigate('Main', {screen: 'FeedDetail', params: data})
@@ -118,7 +131,8 @@ function Feeds ({ navigation }) {
         // console.log(data.fileType, data.fileName)
         fileUrl = `${url}file/${(data.fileType === 'IMAGE') ? 'IMAGE' : 'video'}/${data.fileName}`
         fileType = data.fileType
-        console.log(fileUrl)
+        // console.log('oneMedia', fileUrl)
+        console.log('oneMedia create')
       }
 
       return (
@@ -174,30 +188,35 @@ function Feeds ({ navigation }) {
       <>
         {refreshing ? <ActivityIndicator style={{ zIndex: 100 }}/> : null}
         <Animated.FlatList
-        data={feeds}
-        renderItem={onefeed}
-        style={{
-          backgroundColor: "white",
-          transform: [{ translateY: translateY }],
-          elevation: 8,
-        }}
-        nestedScrollEnabled
-        
-        onEndReachedThreshold = {0.1}
-        onEndReached={ checkfeed }
-        
-        refreshing
-        refreshControl={
-          <RefreshControl refreshing={refreshing} onRefresh={refresh} />
-        }
-        
-        ListEmptyComponent={
-          <View><Text>새로운 이야기를 채워주세요</Text></View>
-        }
-        
-        // 보통 여기에 로딩(원 도는거 넣는다는데...)
-        // ListFooterComponent={}
-        ></Animated.FlatList>
+          data={feeds}
+          renderItem={onefeed}
+          style={{
+            backgroundColor: "white",
+            transform: [{ translateY: translateY }],
+            elevation: 8,
+          }}
+          nestedScrollEnabled
+          
+          onEndReachedThreshold = {0.3}
+          onEndReached={ checkfeed }
+          keyExtractor={item => item.boardId}
+
+          showsVerticalScrollIndicator={false}
+          
+          refreshing
+          refreshControl={
+            <RefreshControl refreshing={refreshing} onRefresh={refresh} />
+          }
+          
+          ListEmptyComponent={
+            <View><Text style={{ textAlign: 'center' }}>새로운 이야기를 채워주세요</Text></View>
+          }
+          
+          // 보통 여기에 로딩(원 도는거 넣는다는데...)
+          ListFooterComponent={
+            !(noMoreFeed) ? <ActivityIndicator /> : <View><Text style={{ textAlign: 'center' }}>새로운 이야기를 채워주세요</Text></View>
+          }
+        />
       </>
     )
   }
